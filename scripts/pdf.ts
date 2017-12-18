6fc4257393ce6a512d2a1ab0d5543fc94906f86a
Shuma Yoshioka <s64.stdio@gmail.com>
2017-12-18 13:38:59 +0900

    Create `build:pdf` script

---
+++ scripts/pdf.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as pdf from 'html-pdf';
+import * as path from 'path';
+
+const html = fs.readFileSync(
+  path.join(__dirname, '../dist/index.html'),
+  {
+    encoding: 'utf8',
+  }
+);
+
+const options: pdf.CreateOptions = {
+  format: 'A4',
+  orientation: 'portrait',
+  type: 'pdf'
+};
+
+pdf.create(html, options).toFile(
+  path.join(__dirname, '../dist/index.pdf'),
+  (err: Error, res: pdf.FileInfo): void => {
+    if (err) {
+      throw err;
+    }
+  }
+);
